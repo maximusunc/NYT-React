f0320fe9c2b83da31832f46c49c03f02fa84eea9
Max Wang <maxwellswang@gmail.com>
2018-02-01 18:32:54 -0500

    testing

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
+  // app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 // Set up promises with mongoose
@@ -29,9 +29,9 @@ mongoose.connect(
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
